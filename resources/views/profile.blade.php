df35b404f76d5b6512546a0aa864b6591605b465
Sivagnanam Gnanatheeban <theeban4evolve@gmail.com>
2023-08-28 12:20:08 +0530

    profile edit

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -40,7 +40,7 @@
                     <div class="row mb-2">
                         <div class="col-sm-6">
                             <h1>Welcome,
-                                @foreach($data as $ud)
+                                @foreach($userdata as $ud)
                                     {{ $ud->firstname }} {{ $ud->lastname }}
                                 @endforeach
                             </h1>
@@ -49,7 +49,7 @@
                             <ol class="breadcrumb float-sm-right">
                                 <li class="breadcrumb-item"><a href="#">Home</a></li>
                                 <li class="breadcrumb-item active">Welcome, 
-                                    @foreach($data as $ud)
+                                    @foreach($userdata as $ud)
                                         {{ $ud->firstname }} {{ $ud->lastname }}
                                     @endforeach
                                 </li>
